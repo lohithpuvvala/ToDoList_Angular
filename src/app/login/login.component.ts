fff83f8eeec426afb77f9138c2f2207c5b108968
lohithpuvvala <lohithpuvvala@gmail.com>
2025-01-06 16:33:27 +0530

    Connected router from Login to Welcome page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgIf } from '@angular/common';
 import { Router } from '@angular/router';
+import { WelcomeComponent } from '../welcome/welcome.component';
 
 @Component({
   selector: 'app-login',
@@ -19,15 +20,14 @@ export class LoginComponent {
   password = ''
   errorMessage='Invalid Credentials'
   invalidLogin=false;
+  
 
-  constructor(router: Router){
-
-  }
+  constructor(private router: Router){ }
 
   handleLogin(){
     if(this.username==='lohithpuvvala' && this.password==='dummy')
     {
-
+      this.router.navigate(['welcome'])
       this.invalidLogin = false;
     }else{
       this.invalidLogin = true;
