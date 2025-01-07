9826696c9bf434cc37a6c0bd73e722e577badfbe
lohithpuvvala <lohithpuvvala@gmail.com>
2025-01-07 17:28:39 +0530

    Updated App Route: User can only open pages when logged in

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,14 +5,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ErrorComponent } from './error/error.component';
+import { RouteGaurdService } from './service/route-gaurd.service';
 
 // welcome 
 export const routes: Routes = [
   { path: '', component: LoginComponent  },//canActivate, RouteGuardService
   { path: 'login', component: LoginComponent },
-  { path: 'welcome/:name', component: WelcomeComponent},
-  { path: 'todos', component: ListTodosComponent},
-  { path: 'logout', component: LogoutComponent},
+  { path: 'welcome/:name', component: WelcomeComponent, canActivate:[RouteGaurdService]},
+  { path: 'todos', component: ListTodosComponent, canActivate:[RouteGaurdService]},
+  { path: 'logout', component: LogoutComponent, canActivate:[RouteGaurdService]},
 
   { path: '**', component: ErrorComponent }
 ];
